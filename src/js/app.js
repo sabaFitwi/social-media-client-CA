f9eae619cab0e459d0626c13be61b9a9be68b043
sabaFitwi <fitwisaba@gmail.com>
2022-11-04 13:34:07 +0100

    bad commit

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+console.log("Intentional Syntax Error");
+
+let i = 5;
+i = 3;
+console.log(i);
+export const secondeTimer = () => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
+};
