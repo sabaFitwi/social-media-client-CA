bd58849622390a5f15cb757cca186acc641c1cae
sabaFitwi <fitwisaba@gmail.com>
2022-11-09 21:38:56 +0100

    fixed small code

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -10,9 +10,7 @@ describe("social media app", () => {
     cy.get("header [data-auth='login']").should("be.visible").click();
 
     cy.wait(800);
-    cy.get(
-      "form#loginForm  div.modal-body div.form-floating  input[type='email']"
-    )
+    cy.get("form#loginForm    input[type='email']")
       .should("be.visible")
       .type("saba.samuel@noroff.no")
       .should("have.value", "saba.samuel@noroff.no");
@@ -20,8 +18,8 @@ describe("social media app", () => {
       "form#loginForm  div.modal-body  div.form-floating > input[type='password']"
     )
       .should("be.visible")
-      .type("Tobi1234")
-      .should("have.value", "Tobi1234");
+      .type("Fitwi001")
+      .should("have.value", "Fitwi001");
     cy.get("form#loginForm > div.modal-footer > button.btn-success")
       .contains("Login")
       .should("be.visible")
@@ -32,10 +30,10 @@ describe("social media app", () => {
   // The create item form validates user inputs correctly based on API restrictions
   it(" create item form validates user inputs correctly based on API restrictions", () => {
     cy.wait(800);
-    cy.visit("http://127.0.0.1:5500/?view=post");
+    cy.visit("/?view=post");
     cy.get("#footerActions > a.btn")
       .contains("New Post")
-      .should("be.visible", { force: true })
+      .should("be.visible")
       .click({ force: true });
     cy.get("form#postForm  input[name='title']")
       .should("be.visible")
@@ -44,11 +42,11 @@ describe("social media app", () => {
     cy.get("form#postForm   input[name='tags']")
       .should("be.visible")
       .type("cypress");
-    cy.get("form#postForm    input[name='media']")
+    cy.get("form#postForm input[name='media']")
       .should("be.visible")
       .type("https://picsum.photos/id/111/4400/2656");
 
-    cy.get("form#postForm     textarea[name ='body']")
+    cy.get("form#postForm textarea[name ='body']")
       .should("be.visible")
       .type("Test cypress");
     cy.get("form#postForm > div.col-12 > button.btn-success")
